ab7da0c89603259c0e19faf9960bd44712b3c59c
Cole Savage <colesavage02@gmail.com>
2018-02-10 21:28:24 -0800

    Add files via upload

--- Training Arena/tools/python/desc_format.py
+++ Training Arena/tools/python/desc_format.py
@@ -2,10 +2,11 @@ import sys
 import os
 import fileinput
 
-if len(sys.argv) < 2:   
-    os.system("cls")
-    print("You must supply a file name as an argument when running this program.")
+if len(sys.argv) < 3:
+    print("You must supply a file name and a prefix as an argument when running this program.")
     sys.exit(2)
 
+prefix = sys.argv[2]
+
 for line in fileinput.input(sys.argv[1], inplace=True):
-    sys.stdout.write('positive_images/{l}'.format(l=line))
+    sys.stdout.write(prefix+'/{l}'.format(l=line))
